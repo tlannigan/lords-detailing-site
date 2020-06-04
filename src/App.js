5a2cb7f35d90e26389ce4283071c2cab5299c70c
tlannigan <tristanlannigan@gmail.com>
2020-06-04 15:29:28 -0600

    Add create-react-app and styled-components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+
+`
+
+function App() {
+  return (
+    <StyledApp className="App">
+      
+    </StyledApp>
+  );
+}
+
+export default App;
